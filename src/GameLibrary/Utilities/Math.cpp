5359fa8ba5973d260c7a33c01d6c51a66480076b
Joseph Hinkle <joehinkle11@gmail.com>
2014-11-14 02:05:23 -0500

    fix utilities folder name

--- src/GameLibrary/Utilities/Math.cpp
+++ src/GameLibrary/Utilities/Math.cpp
@@ -1,5 +1,8 @@
 
+
+#include "Math.h"
 #include <cmath>
+
 
 
 namespace GameLibrary
--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,5 @@
 
-#include <iostream>
-#include "GameLibrary/Utilities/Math.h"
 
 int main(int argc, char *argv[])
 {
-	std::cout << Math::sin(5);
 }
