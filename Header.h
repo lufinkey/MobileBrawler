ad97374161a3756bfd04c3c286e7c63edfe1ca8a
Joseph Hinkle <joehinkle11@gmail.com>
2014-11-14 01:41:46 -0500

    Math.h

---
+++ Header.h
@@ -0,0 +1,31 @@
+
+#pragma once
+
+namespace GameLibrary
+{
+	class Math
+	{
+	public:
+		double sin(double radians);
+
+		double cos(double radians);
+
+		///natural log
+		double log(double radians);
+
+		double log(double radians, int base);
+
+		double log10(double radians);
+
+
+	};
+
+}
+
+
+
+
+
+
+
+
