65c156ce3616d19f474a80c8c23360e39a1686bd
Joseph Hinkle <joehinkle11@gmail.com>
2014-11-16 15:09:58 -0500

    finished essential math funcs

--- src/GameLibrary/Utilities/Math.cpp
+++ src/GameLibrary/Utilities/Math.cpp
@@ -4,9 +4,79 @@
 
 namespace GameLibrary
 {
+	//Trig
 	double Math::sin(double radians)
 	{
 		return std::sin(radians);
 	}
+	double Math::cos(double radians)
+	{
+		return std::cos(radians);
+	}
+	double Math::tan(double radians)
+	{
+		return std::tan(radians);
+	}
+	double Math::asin(double x)
+	{
+		return std::asin(x);
+	}
+	double Math::acos(double x)
+	{
+		return std::acos(x);
+	}
+	double Math::atan(double x)
+	{
+		return std::atan(x);
+	}
+	double Math::atan2(double x, double y)
+	{
+		return std::atan2(x, y);
+	}
+
+	//Logarithmic
+	double Math::log(double arg)
+	{
+		return std::log(arg);
+	}
+	double Math::log(double base, double arg)
+	{
+		return std::log(arg) / std::log(base);
+	}
+	double Math::log10(double arg)
+	{
+		return std::log10(arg);
+	}
+
+	//Powers
+	double Math::pow(double base, double exponent)
+	{
+		return std::pow(base, exponent);
+	}
+	double Math::sqrt(double arg)
+	{
+		return std::sqrt(arg);
+	}
+
+	//Rounding and remainders
+	double Math::ceil(double arg)
+	{
+		return std::ceil(arg);
+	}
+	double Math::floor(double arg)
+	{
+		return std::floor(arg);
+	}
+	double Math::round(double arg)
+	{
+		return std::round(arg);
+	}
+
+	//Others
+	double Math::abs(double arg)
+	{
+		return std::abs(arg);
+	}
+
 }
 
--- src/GameLibrary/Utilities/Math.h
+++ src/GameLibrary/Utilities/Math.h
@@ -6,12 +6,31 @@ namespace GameLibrary
 	class Math
 	{
 	public:
+		//Trig
 		static double sin(double radians);
 		static double cos(double radians);
+		static double tan(double radians);
+		static double asin(double x);
+		static double acos(double x);
+		static double atan(double x);
+		static double atan2(double x, double y);
 
+		//Logarithmic
 		static double log(double arg); //natural log
 		static double log(double base, double arg);
 		static double log10(double arg);
+
+		//Power
+		static double pow(double base, double exponent);
+		static double sqrt(double arg);
+
+		//Rounding and remainders
+		static double ceil(double arg);
+		static double floor(double arg);
+		static double round(double arg);
+
+		//Others
+		static double abs(double arg);
 	};
 }
 
