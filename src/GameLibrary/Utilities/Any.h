ac5f9ea65eb26769d33020b6cd4a5a0dc629385f
lufinkey <luisfinke@gmail.com>
2014-11-16 23:49:21 -0500

    added implementation for "Any" class

---
+++ src/GameLibrary/Utilities/Any.h
@@ -0,0 +1,156 @@
+
+#include "../Exception/BadAnyCastException.h"
+
+#pragma once
+
+namespace GameLibrary
+{
+	template<class T>
+	using StorageType = typename decay<T>::type;
+	class Any
+	{
+	private:
+		struct Base
+		{
+			virtual ~Base()
+			{
+				//
+			}
+
+			virtual Base* clone() const = 0;
+		};
+
+		template<typename T>
+		struct Derived : Base
+		{
+			T value;
+
+			template<typename U> Derived(U&& value) : value(forward<U>(value))
+			{
+				//
+			}
+
+			Base* clone() const
+			{
+				return new Derived<T>(value);
+			}
+		};
+
+		Base* clone() const
+		{
+			if (ptr)
+			{
+				return ptr->clone();
+			}
+			else
+			{
+				return nullptr;
+			}
+		}
+
+		Base* ptr;
+
+	public:
+		Any() : ptr(nullptr)
+		{
+			//
+		}
+
+		Any(Any& any) : ptr(any.clone())
+		{
+			//
+		}
+
+		Any(Any&& any) : ptr(any.ptr)
+		{
+			any.ptr = nullptr;
+		}
+
+		Any(const Any& any) : ptr(any.clone())
+		{
+			//
+		}
+
+		Any(const Any&& any) : ptr(any.clone())
+		{
+			//
+		}
+
+		template<class U>
+		operator U()
+		{
+			return as<StorageType<U>>();
+		}
+
+		Any& operator=(const Any& any)
+		{
+			if (ptr == any.ptr)
+			{
+				return *this;
+			}
+			auto old_ptr = ptr;
+			ptr = any.clone();
+
+			if (old_ptr)
+			{
+				delete old_ptr;
+			}
+
+			return *this;
+		}
+
+		Any& operator=(Any&& any)
+		{
+			if (ptr == any.ptr)
+			{
+				return *this;
+			}
+
+			std::swap(ptr, any.ptr);
+
+			return *this;
+		}
+
+		~Any()
+		{
+			if (ptr)
+			{
+				delete ptr;
+			}
+		}
+
+		bool is_null() const
+		{
+			return !ptr;
+		}
+
+		bool not_null() const
+		{
+			return ptr;
+		}
+
+		template<typename U> Any(U&& value) : ptr(new Derived<StorageType<U>>(forward<U>(value)))
+		{
+			//
+		}
+
+		template<class U> bool is() const
+		{
+			typedef StorageType<U> T;
+			auto derived = dynamic_cast<Derived<T>*> (ptr);
+			return derived;
+		}
+
+		template<class U>
+		StorageType<U>& as()
+		{
+			typedef StorageType<U> T;
+			auto derived = dynamic_cast<Derived<T>*>(ptr);
+			if (!derived)
+			{
+				throw BadAnyCastException(typeid(U).name());
+			}
+			return derived->value;
+		}
+	};
+}
