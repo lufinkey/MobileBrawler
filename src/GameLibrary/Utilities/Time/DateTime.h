4ac8039951fb1fd358bc451e670079032a3e6b32
lufinkey <luisfinke@gmail.com>
2014-11-27 20:43:30 -0500

    Finished DateTime class (for now)
    DateTime will be immutable for now, until I can find a good way to
    implement time zones

--- src/GameLibrary/Utilities/Time/DateTime.h
+++ src/GameLibrary/Utilities/Time/DateTime.h
@@ -1,5 +1,7 @@
 
 #include "../String.h"
+#include "../Dictionary.h"
+#include "../../Exception/IllegalDateTimeFormatException.h"
 
 #pragma once
 
@@ -8,51 +10,66 @@ namespace GameLibrary
 	class DateTime
 	{
 	private:
+		/// UTC offset in seconds (00000-43200)
+		long utc_offset;
+
+		/// microsecond (000000-999999)
 		int usec;
+
+		/// second (00-61) (generally 00-59. the extra 2 seconds are for leap seconds)
 		int sec;
+
+		/// minute (00-59)
 		int min;
+
+		/// hour (00-23)
 		int hour;
+
+		/// day of the month (01-31)
 		int mday;
-		int wday;
+
+		/// month (01-12)
 		int mon;
-		int yday;
+
+		/// year (0000-9999)
 		int year;
-		int utc_minoff;
-		int utc_houroff;
-		
+
+		/// day of the week (sunday=1) (1-7)
+		int wday;
+
+		/// day of the year (001-366)
+		int yday;
+
 	public:
-		DateTime(); //new datetime object with current time
+		DateTime(); //new datetime object with current date/time
 		DateTime(const DateTime&);
-		DateTime(const String&datestring, const String&format=String("%Y-%m-%d %H:%M:%S %z"));
+		/*DateTime(const String&datestring, const String&format=String("%Y-%m-%d %H:%M:%S %z"));
 		DateTime(int year, int mon, int day);
-		DateTime(int year, int mon, int day, int hour, int min, int sec, int usec);
+		DateTime(int year, int mon, int day, int hour, int min, int sec, int usec);*/ //DateTime is immutable for now
 		~DateTime();
 		
-		int getMicroseconds();
-		int getSeconds();
-		int getMinutes();
-		int getHour();
-		int getDayOfMonth();
-		int getDayOfWeek();
-		int getDayOfYear();
-		int getMonth();
-		int getYear();
-		int getMinuteOffsetUTC();
-		int getHourOffsetUTC();
+		int getMicrosecond() const;
+		int getSecond() const;
+		int getMinute() const;
+		int getHour() const;
+		int getDayOfMonth() const;
+		int getDayOfWeek() const;
+		int getDayOfYear() const;
+		int getMonth() const;
+		int getYear() const;
+		long getOffsetUTC() const;
 
-		String getDayOfWeekString();
-		String getMonthString();
-
-		String toString(const String&format=String("%Y-%m-%d %H:%M:%S %z"));
+		String toString(const String&format=String("%Y-%m-%d %H:%M:%S %z")) const;
 		
-		void setMicroseconds(int usec);
-		void setSeconds(int sec);
-		void setMinutes(int min);
+		/*void setMicrosecond(int usec);
+		void setSecond(int sec);
+		void setMinute(int min);
 		void setHour(int hour);
 		void setDayOfMonth(int mday);
 		void setDayOfWeek(int wday);
 		void setDayOfYear(int yday);
 		void setMonth(int mon);
 		void setYear(int year);
+		void setOffsetUTC(long utc_offset);*/ //DateTime is immutable for now
 	};
 }
