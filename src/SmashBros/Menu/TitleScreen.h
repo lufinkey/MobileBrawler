4a10f074a823672792594d51a481acaecbb7789e
lufinkey <luisfinke@gmail.com>
2015-01-12 00:27:39 -0500

    began work on TitleScreen

---
+++ src/SmashBros/Menu/TitleScreen.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "BaseMenuScreen.h"
+
+namespace SmashBros
+{
+	namespace Menu
+	{
+		class TitleScreen : public SmashBros::Menu::BaseMenuScreen
+		{
+		public:
+			TitleScreen(AssetManager*assetManager);
+			virtual ~TitleScreen();
+		};
+	}
+}
