e65bbc05d658a1d835618c43edef8ad41e4f284c
lufinkey <luisfinke@gmail.com>
2015-01-26 20:37:27 -0500

    added empty GroupRulesMenu class

---
+++ src/SmashBros/Menu/GroupRulesMenu.h
@@ -0,0 +1,17 @@
+
+#pragma once
+
+#include "Base/BaseMenuScreen.h"
+
+namespace SmashBros
+{
+	namespace Menu
+	{
+		class GroupRulesMenu : public BaseMenuScreen
+		{
+		public:
+			GroupRulesMenu(const SmashData&smashData);
+			virtual ~GroupRulesMenu();
+		};
+	}
+}
