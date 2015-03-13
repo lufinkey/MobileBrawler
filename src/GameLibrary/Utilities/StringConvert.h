3c0aae217fc917f8d4c10c5c64dc59195775c9bd
lufinkey <luisfinke@gmail.com>
2015-03-12 23:03:19 -0400

    More StringConvert fixes

--- src/GameLibrary/Utilities/StringConvert.h
+++ src/GameLibrary/Utilities/StringConvert.h
@@ -47,18 +47,20 @@ namespace GameLibrary
 		{
 			size_t size = src.length();
 			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst;
-			dst.resize(size);
+			dst.resize(size+1);
 			for(size_t i=0; i<size; i++)
 			{
 				dst.at(i) = (D)src.at(i);
 			}
+			dst.at(size) = NULL;
 			return dst;
 		}
 		else if(sizeof(S) > sizeof(D))
 		{
 			size_t size = src.length();
 			S d_max = (S)std::numeric_limits<D>();
-			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst(size);
+			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst;
+			dst.resize(size+1);
 			for(size_t i=0; i<size; i++)
 			{
 				if(src.at(i) >= d_max)
@@ -77,6 +79,7 @@ namespace GameLibrary
 					d.at(i) = (D)src.at(i);
 				}
 			}
+			dst.at(size) = NULL;
 			return dst;
 		}
 		else
@@ -91,64 +94,21 @@ namespace GameLibrary
 		if(sizeof(String::char_type) < sizeof(D))
 		{
 			size_t size = src.length();
-			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst(size);
+			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst;
+			dst.resize(size+1);
 			for(size_t i=0; i<size; i++)
 			{
 				dst.at(i) = (D)src.charAt(i);
 			}
+			dst.at(size) = NULL;
 			return dst;
 		}
 		else if(sizeof(String::char_type) > sizeof(D))
 		{
 			size_t size = src.length();
 			String::char_type d_max = (String::char_type)std::numeric_limits<D>::max();
-			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst(size);
-			for(size_t i=0; i<size; i++)
-			{
-				if(src.charAt(i) >= d_max)
-				{
-					if(sizeof(D) >= 2)
-					{
-						dst.at(i) = (D)0xfffd;
-					}
-					else
-					{
-						dst.at(i) = (D)'?';
-					}
-				}
-				else
-				{
-					d.at(i) = (D)src.charAt(i);
-				}
-			}
-			return dst;
-		}
-		else
-		{
-			return std::basic_string<D, std::char_traits<D>, std::allocator<D> >((const D*)((const String::char_type*)src), ((const D*)((const String::char_type*)src)) + src.length());
-		}
-	}
-	
-	template<typename D>
-	std::basic_string<D, std::char_traits<D>, std::allocator<D> > StringForceConvert(const WideString&src)
-	{
-		if(sizeof(WideString::char_type) < sizeof(D))
-		{
-			size_t size = src.length();
 			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst;
-			dst.resize(size);
-			for(size_t i=0; i<size; i++)
-			{
-				dst.at(i) = (D)src.charAt(i);
-			}
-			return dst;
-		}
-		else if(sizeof(WideString::char_type) > sizeof(D))
-		{
-			size_t size = src.length();
-			WideString::char_type d_max = (WideString::char_type)std::numeric_limits<D>::max();
-			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst;
-			dst.resize(size);
+			dst.resize(size+1);
 			for(size_t i=0; i<size; i++)
 			{
 				if(src.charAt(i) >= d_max)
@@ -167,6 +127,55 @@ namespace GameLibrary
 					dst.at(i) = (D)src.charAt(i);
 				}
 			}
+			dst.at(size) = NULL;
+			return dst;
+		}
+		else
+		{
+			return std::basic_string<D, std::char_traits<D>, std::allocator<D> >((const D*)((const String::char_type*)src), ((const D*)((const String::char_type*)src)) + src.length());
+		}
+	}
+	
+	template<typename D>
+	std::basic_string<D, std::char_traits<D>, std::allocator<D> > StringForceConvert(const WideString&src)
+	{
+		if(sizeof(WideString::char_type) < sizeof(D))
+		{
+			size_t size = src.length();
+			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst;
+			dst.resize(size+1);
+			for(size_t i=0; i<size; i++)
+			{
+				dst.at(i) = (D)src.charAt(i);
+			}
+			dst.at(size) = NULL;
+			return dst;
+		}
+		else if(sizeof(WideString::char_type) > sizeof(D))
+		{
+			size_t size = src.length();
+			WideString::char_type d_max = (WideString::char_type)std::numeric_limits<D>::max();
+			std::basic_string<D, std::char_traits<D>, std::allocator<D> > dst;
+			dst.resize(size+1);
+			for(size_t i=0; i<size; i++)
+			{
+				if(src.charAt(i) >= d_max)
+				{
+					if(sizeof(D) >= 2)
+					{
+						dst.at(i) = (D)0xfffd;
+					}
+					else
+					{
+						dst.at(i) = (D)'?';
+					}
+				}
+				else
+				{
+					dst.at(i) = (D)src.charAt(i);
+				}
+			}
+			dst.at(size) = NULL;
 			return dst;
 		}
 		else
