ac6dcfcb4b7dff5f935c8b27e9bbd4535fb071ad
lufinkey <luisfinke@gmail.com>
2017-04-21 13:34:12 -0400

    put MenuButton in Menu namespace

--- src/SmashBros/Menu/Elements/MenuButton.cpp
+++ src/SmashBros/Menu/Elements/MenuButton.cpp
@@ -3,85 +3,88 @@
 
 namespace SmashBros
 {
+	namespace Menu
+	{
 #define PULSE_UPPERBOUND 1.1
 #define PULSE_LOWERBOUND 0.98
 #define PULSE_SPEED 0.3
-	
-	MenuButton::MenuButton()
-		: MenuButton(nullptr, "")
-	{
-		//
-	}
-	
-	MenuButton::MenuButton(fgl::AssetManager* assetManager, const fgl::String& imagePath)
-		: hoverPulsingEnabled(true)
-	{
-		if(imagePath.length() > 0)
-		{
-			if(assetManager==nullptr)
-			{
-				throw fgl::IllegalArgumentException("assetManager", "cannot be null when specifying an image path");
-			}
-			if(assetManager->loadTexture(imagePath))
-			{
-				setImage(assetManager->getTexture(imagePath), BUTTONSTATE_NORMAL);
-			}
-		}
-		setTintColor(fgl::Color::LIGHTBLUE, BUTTONSTATE_HOVERED);
-		setTintColor(fgl::Color::BLUE, BUTTONSTATE_PRESSED);
-		setTintColor(fgl::Color::LIGHTGRAY, BUTTONSTATE_DISABLED);
 		
-		getImageElement()->setDisplayMode(fgl::ImageElement::DISPLAY_FIT_CENTER);
-	}
-	
-	void MenuButton::update(fgl::ApplicationData appData)
-	{
-		if(getHoveredMouseIndexes().size() > 0 && hoverPulsingEnabled && isEnabled())
+		MenuButton::MenuButton()
+			: MenuButton(nullptr, "")
 		{
-			float scaleIncrement = (float)(PULSE_SPEED * appData.getFrameSpeedMultiplier());
-			if(hoverPulseGrowing)
+			//
+		}
+		
+		MenuButton::MenuButton(fgl::AssetManager* assetManager, const fgl::String& imagePath)
+			: hoverPulsingEnabled(true)
+		{
+			if(imagePath.length() > 0)
 			{
-				hoverPulseScale += scaleIncrement;
-				if(hoverPulseScale >= PULSE_UPPERBOUND)
+				if(assetManager==nullptr)
 				{
-					hoverPulseScale = PULSE_UPPERBOUND;
-					hoverPulseGrowing = false;
+					throw fgl::IllegalArgumentException("assetManager", "cannot be null when specifying an image path");
+				}
+				if(assetManager->loadTexture(imagePath))
+				{
+					setImage(assetManager->getTexture(imagePath), BUTTONSTATE_NORMAL);
+				}
+			}
+			setTintColor(fgl::Color::LIGHTBLUE, BUTTONSTATE_HOVERED);
+			setTintColor(fgl::Color::BLUE, BUTTONSTATE_PRESSED);
+			setTintColor(fgl::Color::LIGHTGRAY, BUTTONSTATE_DISABLED);
+			
+			getImageElement()->setDisplayMode(fgl::ImageElement::DISPLAY_FIT_CENTER);
+		}
+		
+		void MenuButton::update(fgl::ApplicationData appData)
+		{
+			if(getHoveredMouseIndexes().size() > 0 && hoverPulsingEnabled && isEnabled())
+			{
+				float scaleIncrement = (float)(PULSE_SPEED * appData.getFrameSpeedMultiplier());
+				if(hoverPulseGrowing)
+				{
+					hoverPulseScale += scaleIncrement;
+					if(hoverPulseScale >= PULSE_UPPERBOUND)
+					{
+						hoverPulseScale = PULSE_UPPERBOUND;
+						hoverPulseGrowing = false;
+					}
+				}
+				else
+				{
+					hoverPulseScale -= scaleIncrement;
+					if(hoverPulseScale <= PULSE_LOWERBOUND)
+					{
+						hoverPulseScale = PULSE_LOWERBOUND;
+						hoverPulseGrowing = true;
+					}
 				}
 			}
 			else
 			{
-				hoverPulseScale -= scaleIncrement;
-				if(hoverPulseScale <= PULSE_LOWERBOUND)
-				{
-					hoverPulseScale = PULSE_LOWERBOUND;
-					hoverPulseGrowing = true;
-				}
+				hoverPulseScale = 1;
 			}
+			ButtonElement::update(appData);
 		}
-		else
+		
+		void MenuButton::draw(fgl::ApplicationData appData, fgl::Graphics graphics) const
 		{
-			hoverPulseScale = 1;
+			if(getHoveredMouseIndexes().size() > 0 && hoverPulsingEnabled && isEnabled())
+			{
+				auto center = getFrame().getCenter();
+				graphics.scale((double)hoverPulseScale, (double)hoverPulseScale, center.x, center.y);
+			}
+			ButtonElement::draw(appData, graphics);
 		}
-		ButtonElement::update(appData);
-	}
-	
-	void MenuButton::draw(fgl::ApplicationData appData, fgl::Graphics graphics) const
-	{
-		if(getHoveredMouseIndexes().size() > 0 && hoverPulsingEnabled && isEnabled())
+		
+		void MenuButton::setHoverPulsingEnabled(bool enabled)
 		{
-			auto center = getFrame().getCenter();
-			graphics.scale((double)hoverPulseScale, (double)hoverPulseScale, center.x, center.y);
+			hoverPulsingEnabled = enabled;
 		}
-		ButtonElement::draw(appData, graphics);
-	}
-	
-	void MenuButton::setHoverPulsingEnabled(bool enabled)
-	{
-		hoverPulsingEnabled = enabled;
-	}
-	
-	bool MenuButton::isHoverPulsingEnabled() const
-	{
-		return hoverPulsingEnabled;
+		
+		bool MenuButton::isHoverPulsingEnabled() const
+		{
+			return hoverPulsingEnabled;
+		}
 	}
 }
--- src/SmashBros/Menu/Elements/MenuButton.hpp
+++ src/SmashBros/Menu/Elements/MenuButton.hpp
@@ -5,21 +5,24 @@
 
 namespace SmashBros
 {
-	class MenuButton : public fgl::ButtonElement
+	namespace Menu
 	{
-	public:
-		MenuButton();
-		MenuButton(fgl::AssetManager* assetManager, const fgl::String& imagePath);
-		
-		virtual void update(fgl::ApplicationData appData) override;
-		virtual void draw(fgl::ApplicationData appData, fgl::Graphics graphics) const override;
-		
-		void setHoverPulsingEnabled(bool enabled);
-		bool isHoverPulsingEnabled() const;
-		
-	private:
-		bool hoverPulsingEnabled;
-		bool hoverPulseGrowing;
-		float hoverPulseScale;
-	};
+		class MenuButton : public fgl::ButtonElement
+		{
+		public:
+			MenuButton();
+			MenuButton(fgl::AssetManager* assetManager, const fgl::String& imagePath);
+			
+			virtual void update(fgl::ApplicationData appData) override;
+			virtual void draw(fgl::ApplicationData appData, fgl::Graphics graphics) const override;
+			
+			void setHoverPulsingEnabled(bool enabled);
+			bool isHoverPulsingEnabled() const;
+			
+		private:
+			bool hoverPulsingEnabled;
+			bool hoverPulseGrowing;
+			float hoverPulseScale;
+		};
+	}
 }
