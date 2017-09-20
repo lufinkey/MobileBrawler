36604c38b93eb53ecdafefde284ed62ae7e9e6a3
lufinkey <luisfinke@gmail.com>
2017-09-19 22:34:48 -0400

    added codelite project

--- src/SmashBros/main.cpp
+++ src/SmashBros/main.cpp
@@ -5,7 +5,7 @@ using namespace fgl;
 
 int main(int argc, char *argv[])
 {
-	SmashBros::Game* game = new SmashBros::Game();
+	auto game = new SmashBros::Game();
 	int returnVal = game->run(Window::getDefaultSettings(), Application::ORIENTATION_LANDSCAPE);
 	delete game;
 	return returnVal;
